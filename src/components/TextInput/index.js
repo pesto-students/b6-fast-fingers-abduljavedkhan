53be9518028f170b8dd47ff9f9b31f31f7040ab8
abduljavedkhan <abduljaved314@gmail.com>
2021-02-26 00:53:16 +0530

    fix bug

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -5,7 +5,7 @@ const TextInput = ({ text, setText, placeholder = '' }) => {
         <div className="my-4 mx-6">
             <input
             autoFocus
-                className={"bg-gray-500  text-secoundry w-56 h-8 mb-1 rounded-md uppercase text-left pl-4"}
+                className={"bg-gray-500  text-secoundry  rounded-lg w-56 h-8 mb-1 uppercase text-left pl-4"}
                 type="text"
                 placeholder={placeholder}
                 value={text}
